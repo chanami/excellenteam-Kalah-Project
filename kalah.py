6cead58381697100c7fc591fcada3ef81ec72c34
chanami <chanami15@gmail.com>
2018-11-15 18:22:06 +0200

    1.1 Initial Status

--- kalah.py
+++ kalah.py
@@ -1,2 +1,7 @@
+
 class Kalah(object):
-    pass
+    def __init__(self, holes, seeds):
+        self.mankala_board = {"player1": [seeds]*holes +[0], "player2": [seeds]*holes +[0]}
+
+    def status(self):
+        return tuple(self.mankala_board["player1"])+ tuple(self.mankala_board["player2"])
--- test_kalah.py
+++ test_kalah.py
@@ -1,9 +1,21 @@
 import unittest
+from kalah import Kalah
 
 
 class KalahTestCase(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.game = Kalah(6, 4)
+
+    def test_init_status(self):
+        self.assertEqual(self.game.status(), (4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0))
+
+    def tearDown(self):
+        pass
+
+
+
 
 
 if __name__ == '__main__':
     unittest.main()
+

0fcf6e8a5fe6e3a724919294fee4aa0de6bd83a8
chanami <chanami15@gmail.com>
2018-11-19 16:31:22 +0200

    1.2 two simple moves test

--- test_kalah.py
+++ test_kalah.py
@@ -24,10 +24,17 @@ class KalahTestCase(unittest.TestCase):
 
     def test_simple_move(self):
         self.assertEqual(self.game.play(0),"Player 2 plays next")
+        self.assertEqual(self.game.status(), (0, 5, 5, 5, 5, 4, 0, 4, 4, 4, 4, 4, 4, 0))
 
     def test_Crossing_move(self):
         self.assertEqual(self.game.play(3), "Player 2 plays next")
         self.assertEqual(self.game.status(), (4, 4, 4, 0, 5, 5, 1, 5, 4, 4, 4, 4, 4, 0))
+
+    def test_Two_simple_moves(self):
+        self.assertEqual(self.game.play(0), "Player 2 plays next")
+        self.assertEqual(self.game.status(), (0, 5, 5, 5, 5, 4, 0, 4, 4, 4, 4, 4, 4, 0))
+        self.assertEqual(self.game.play(0), "Player 1 plays next")
+        self.assertEqual(self.game.status(), (0, 5, 5, 5, 5, 4, 0, 0, 5, 5, 5, 5, 4, 0))
 
     def tearDown(self):
         pass
